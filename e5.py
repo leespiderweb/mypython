b9462a31a9b7aefd8bf5afc5827de779a89fa9fc
leespiderweb <leeviking@126.com>
2018-08-03 20:49:41 +0800

    dev.txt to be commit

---
+++ e5.py
@@ -0,0 +1,20 @@
+#e4.1TextProgressBar.py
+import time
+
+scale = 50
+
+print("打印开始".center(scale//2,'-'))
+t =  time.clock()
+
+for i  in  range(scale+1):
+    a,b = "*" * i, "." * (scale -i)
+    c = i/(scale+1)*100
+    #c = (c+(1-c)**0.03)**2
+    t-= time.clock()
+
+    print("\r{:^3.0f}%, [{} ->{}]  {:.2f}s ".format(c,a,b,-t) ,end="")
+    time.sleep(0.05)
+
+
+print("\n"+"打印结束".center(scale//2,"-"))
+print("test of e5 in statu of test")
